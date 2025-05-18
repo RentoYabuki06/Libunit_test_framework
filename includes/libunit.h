eda78e23399fdbd862c7206e1eab43cf31a47a1f
rento yabuki <rento.nky.1203@gmail.com>
2025-05-18 19:40:43 +0900

    Update libunit.h with struct definition and function prototypes for unit testing

--- includes/libunit.h
+++ includes/libunit.h
@@ -6,7 +6,23 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 18:28:43 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/05/18 18:28:44 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/05/18 18:37:23 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBUNIT_H
+# define LIBUNIT_H
+
+#include <stdio.h>
+
+typedef struct s_unit_test
+{
+	char*				name;
+	int					(*test_func)(void);
+	struct s_unit_test	*next;
+}	t_unit_test;
+
+void	load_test(t_unit_test **list, char *name, int (*test_func)(void));
+int		launch_tests(t_unit_test *list);
+
+#endif
