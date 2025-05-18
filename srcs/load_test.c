9846147c7fd5b705482b259200529fe5506aa4b1
rento yabuki <rento.nky.1203@gmail.com>
2025-05-18 19:17:26 +0900

    Implement load_test function and create_unit_test_node for managing unit tests

--- srcs/load_test.c
+++ srcs/load_test.c
@@ -6,7 +6,61 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 18:28:47 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/05/18 18:28:48 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/05/18 19:17:02 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libunit.h"
+#include "libft.h"
+
+static t_unit_test	*create_unit_test_node(char *name, int (*test_func)(void))
+{
+	t_unit_test *new;
+
+	new = (t_unit_test *)malloc(sizeof(t_unit_test));
+	if (new == NULL)
+	{
+		ft_putstr_fd("Error: malloc failed!\n", STDERR_FILENO);
+		return ;
+	}
+	new->name = ft_strdup(name);
+	if (new->name == NULL)
+	{
+		free(new);
+		new = NULL;
+		ft_putstr_fd("Error: strdup failed!\n", STDERR_FILENO);
+		return ;
+	}
+	new->test_func = test_func;
+	new->next = NULL;
+	return (new);
+}
+
+void	load_test(t_unit_test **list, char *name, int (*test_func)(void))
+{
+	t_unit_test	*new;
+	t_unit_test	*tmp;
+
+	if (list == NULL)
+	{
+		ft_putstr_fd("Error: test list is NULL!\n", STDERR_FILENO);
+		return ;
+	}
+	if (test_func == NULL)
+	{
+		ft_putstr_fd("Error: test_func is NULL!\n", STDERR_FILENO);
+		return ;
+	}
+	new = create_unit_test_node(name, test_func);
+	if (new == NULL)
+		return ;
+	if (*list == NULL)
+		*list = new;
+	else
+	{
+		tmp = *list;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}
