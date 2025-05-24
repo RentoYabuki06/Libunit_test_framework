adf0182c2116353382290042a8372f3102403588
Rento Yabuki <ryabuki@c1r7s7.42tokyo.jp>
2025-05-24 16:56:07 +0900

    norm

--- bonus/srcs/launch_tests_bonus.c
+++ bonus/srcs/launch_tests_bonus.c
@@ -16,99 +16,98 @@
 
 static void	print_result(char *name, int status)
 {
-    int	sig;
+	int	sig;
 
-    if (WIFEXITED(status) == 0)
-    {
-        sig = WTERMSIG(status);
-        if (sig == SIGSEGV)
-            ft_printf("%s[SEGV]%s :", COLOR_SIG, RESET);
-        else if (sig == SIGBUS)
-            ft_printf("%s[BUSE]%s :", COLOR_SIG, RESET);
-        else if (sig == SIGABRT)
-            ft_printf("%s[ABRT]%s :", COLOR_SIG, RESET);
-        else if (sig == SIGFPE)
-            ft_printf("%s[FPE]%s :", COLOR_SIG, RESET);
-        else if (sig == SIGPIPE)
-            ft_printf("%s[PIPE]%s :", COLOR_SIG, RESET);
-        else if (sig == SIGILL)
-            ft_printf("%s[ILL]%s :", COLOR_SIG, RESET);
-        else if (sig == SIGALRM)
-            ft_printf("%s[TIME]%s :", COLOR_SIG, RESET);
-        else
-            ft_printf("%s[SIG %d]%s :", COLOR_SIG, sig, RESET);
-    }
-    else if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
-        ft_printf("%s[OK]  %s :", COLOR_OK, RESET);
-    else
-        ft_printf("%s[KO]  %s :", COLOR_KO, RESET);
-    ft_printf("%s\n", name);
+	if (WIFEXITED(status) == 0)
+	{
+		sig = WTERMSIG(status);
+		if (sig == SIGSEGV)
+			ft_printf("%s[SEGV]%s :", COLOR_SIG, RESET);
+		else if (sig == SIGBUS)
+			ft_printf("%s[BUSE]%s :", COLOR_SIG, RESET);
+		else if (sig == SIGABRT)
+			ft_printf("%s[ABRT]%s :", COLOR_SIG, RESET);
+		else if (sig == SIGFPE)
+			ft_printf("%s[FPE]%s :", COLOR_SIG, RESET);
+		else if (sig == SIGPIPE)
+			ft_printf("%s[PIPE]%s :", COLOR_SIG, RESET);
+		else if (sig == SIGILL)
+			ft_printf("%s[ILL]%s :", COLOR_SIG, RESET);
+		else if (sig == SIGALRM)
+			ft_printf("%s[TIME]%s :", COLOR_SIG, RESET);
+		else
+			ft_printf("%s[SIG %d]%s :", COLOR_SIG, sig, RESET);
+	}
+	else if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+		ft_printf("%s[OK]  %s :", COLOR_OK, RESET);
+	else
+		ft_printf("%s[KO]  %s :", COLOR_KO, RESET);
+	ft_printf("%s\n", name);
 }
 
 static void	print_final_result(int count_success, int count_tests)
 {
-    if (count_success == count_tests)
-        ft_printf("\n%s%d/%d tests passed.%s\n", COLOR_OK, count_success, count_tests, RESET);
-    else
-        ft_printf("\n%s%d/%d tests passed.%s\n", COLOR_KO, count_success, count_tests, RESET);
+	if (count_success == count_tests)
+		ft_printf("\n%s%d/%d tests passed.%s\n", COLOR_OK, count_success, count_tests, RESET);
+	else
+		ft_printf("\n%s%d/%d tests passed.%s\n", COLOR_KO, count_success, count_tests, RESET);
 }
 
 static void alarm_handler(int sig)
 {
-    (void)sig;
+	(void)sig;
 }
 
 static int wait_with_alarm(pid_t pid, int *status, int timeout_sec)
 {
-    struct sigaction sa, old_sa;
-    pid_t result;
-    int was_timeout = 0;
+	struct sigaction sa, old_sa;
+	pid_t result;
+	int was_timeout = 0;
 
-    sa.sa_handler = alarm_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGALRM, &sa, &old_sa);
-    alarm(timeout_sec);
-    result = waitpid(pid, status, 0);
-    if (alarm(0) == 0 && result == -1 && errno == EINTR)
-    {
-        was_timeout = 1;
-        usleep(10000);
-        waitpid(pid, status, WNOHANG);
-    }
-    sigaction(SIGALRM, &old_sa, NULL);
-    return was_timeout ? -1 : result;
+	sa.sa_handler = alarm_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGALRM, &sa, &old_sa);
+	alarm(timeout_sec);
+	result = waitpid(pid, status, 0);
+	if (alarm(0) == 0 && result == -1 && errno == EINTR)
+	{
+		was_timeout = 1;
+		usleep(10000);
+		waitpid(pid, status, WNOHANG);
+	}
+	sigaction(SIGALRM, &old_sa, NULL);
+	return was_timeout ? -1 : result;
 }
 
 int	launch_tests(t_unit_test *list)
 {
-    pid_t	pid;
-    int		status;
-    int		count_success;
-    int		count_tests;
+	pid_t	pid;
+	int		status;
+	int		count_success;
+	int		count_tests;
 
-    count_success = 0;
-    count_tests = 0;
-    while (list)
-    {
-        pid = fork();
-        if (pid == 0)
-        {
-            // 子プロセスではアラームをリセット
-            signal(SIGALRM, SIG_DFL);
-            exit(list->test_func());
-        }
-        if (wait_with_alarm(pid, &status, TIMEOUT_SECONDS) == -1)
-            ft_printf("%s[TIME]%s :%s (exceeded %d seconds)\n", COLOR_SIG, RESET, list->name, TIMEOUT_SECONDS);
-        else
-            print_result(list->name, status);
-        if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
-            count_success++;
-        list = list->next;
-        count_tests++;
-    }
-    print_final_result(count_success, count_tests);
-    if (count_success == count_tests)
-        return (0);
-    return (-1);
+	count_success = 0;
+	count_tests = 0;
+	while (list)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			signal(SIGALRM, SIG_DFL);
+			exit(list->test_func());
+		}
+		if (wait_with_alarm(pid, &status, TIMEOUT_SECONDS) == -1)
+			ft_printf("%s[TIME]%s :%s (exceeded %d seconds)\n", COLOR_SIG, RESET, list->name, TIMEOUT_SECONDS);
+		else
+			print_result(list->name, status);
+		if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+			count_success++;
+		list = list->next;
+		count_tests++;
+	}
+	print_final_result(count_success, count_tests);
+	if (count_success == count_tests)
+		return (0);
+	return (-1);
 }
