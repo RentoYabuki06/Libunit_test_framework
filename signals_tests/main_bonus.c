6043b3987a689beac1db811da0cf789d7a84afea
RentoYabuki06 <rento.nky.1203@gmail.com>
2025-05-25 16:45:10 +0900

    norm: signal_tests dir

--- signals_tests/main_bonus.c
+++ signals_tests/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 16:45:10 by ryabuki           #+#    #+#             */
-/*   Updated: 2025/05/25 15:50:23 by ryabuki          ###   ########.fr       */
+/*   Updated: 2025/05/25 16:44:56 by ryabuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,14 @@
 
 int	main(void)
 {
-    int	result;
+	int	result;
 
-    result = 0;
-    ft_printf("==== SIGNAL HANDLING BONUS TESTS ====\n\n");
-    
-    result |= signals_launcher();
-    
-    // Print summary
-    if (result == 0)
-        ft_printf("\nAll signal tests passed!\n");
-    else
-        ft_printf("\nSome signal tests failed!\n");
-    
-    return (result);
+	result = 0;
+	ft_printf("==== SIGNAL HANDLING BONUS TESTS ====\n\n");
+	result |= signals_launcher();
+	if (result == 0)
+		ft_printf("\nAll signal tests passed!\n");
+	else
+		ft_printf("\nSome signal tests failed!\n");
+	return (result);
 }
